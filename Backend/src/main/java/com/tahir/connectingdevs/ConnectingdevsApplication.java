5b406a6a8e240f3ac56f00117916ad63b3196f3c
Tahir iqbal <shahtahir786@gmail.com>
2024-02-02 11:06:11 +0530

    setting up backend

---
+++ Backend/src/main/java/com/tahir/connectingdevs/ConnectingdevsApplication.java
@@ -0,0 +1,13 @@
+package com.tahir.connectingdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingdevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingdevsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/tahir/connectingdevs/ConnectingdevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahir.connectingdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectingdevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
